8b3ac0b8eb401f78cd2d8bb405c95cff040b27b1
Christian Sheen <christiansheen@Christians-MacBook-Pro.local>
2020-06-03 10:42:27 -0400

    first commit after a while

---
+++ pods.js
@@ -0,0 +1,81 @@
+export const page1 = {
+	data: [
+		{
+			id: "01",
+			Name: "Work",
+			Participants: "12",
+			Date: "11/12/2016",
+		},
+		{
+			id: "02",
+			Name: "Family",
+			Participants: "5",
+			Date: "1/5/2017",
+		},
+		{
+			id: "03",
+			Name: "Ladies",
+			Participants: "3",
+			Date: "5/1/2017",
+		},
+		{
+			id: "04",
+			Name: "Gamers",
+			Participants: "9",
+			Date: "4/12/2018",
+		},
+		{
+			id: "05",
+			Name: "Dojo",
+			Participants: "10",
+			Date: "11/11/2020",
+		},
+		{
+			id: "06",
+			Name: "WE",
+			Participants: "12",
+			Date: "11/12/2016",
+		},
+	],
+};
+
+export const page2 = {
+	data: [
+		{
+			id: "07",
+			Name: "Work22",
+			Participants: "12",
+			Date: "11/12/2016",
+		},
+		{
+			id: "08",
+			Name: "Family22",
+			Participants: "5",
+			Date: "1/5/2017",
+		},
+		{
+			id: "034",
+			Name: "Ladies22",
+			Participants: "3",
+			Date: "5/1/2017",
+		},
+		{
+			id: "044",
+			Name: "Gamers22",
+			Participants: "9",
+			Date: "4/12/2018",
+		},
+		{
+			id: "054",
+			Name: "Dojo22",
+			Participants: "10",
+			Date: "11/11/2020",
+		},
+		{
+			id: "064",
+			Name: "WE22",
+			Participants: "12",
+			Date: "11/12/2016",
+		},
+	],
+};
--- src/home.js
+++ src/home.js
@@ -1,38 +1,54 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import {
-	View,
 	StyleSheet,
-	ScrollView,
+	Text,
+	View,
+	FlatList,
 	TouchableOpacity,
-	Button,
 } from "react-native";
 
-export default function AppHome({ navigation }) {
+import { page1, page2 } from "../pods";
+
+const podIcon = require("./images/chemistryPod.png");
+
+export default function AppHome() {
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setData(page1.data);
+		}, 1500);
+	}, []);
+
+	const nextPage = () => {
+		setData([...data, page2.data]);
+	};
+
 	return (
-		<View style={{ height: 808, backgroundColor: "#E8E8E8" }}>
-			<View style={styles.innerContainerHome}>
-				<View style={styles.Sview}>
-					<ScrollView
+		<View style={styles.baseLayout}>
+			<FlatList
+				data={data}
+				keyExtractor={(item) => item.id}
+				renderItem={({ item }) => (
+					<Text
 						style={{
 							backgroundColor: "#E8E8E8",
+							height: 50,
+							shadowOffset: { width: 0, height: 3 },
+							shadowColor: "#c0c0c0",
+							shadowOpacity: 1.0,
 						}}
-						contentContainerStyle={{ width: 680 }}
-						horizontal={true}
-						showsHorizontalScrollIndicator={false}
-						scrollEventThrottle={10}
-						pagingEnabled
 					>
-						<TouchableOpacity style={styles.moodCard}>
-							<Button
-								title="Happy"
-								onPress={() => navigation.navigate("Mood Card")}
-							/>
-						</TouchableOpacity>
-					</ScrollView>
-				</View>
-				<View style={styles.mainContainer}></View>
-				<View style={styles.baselineButton}></View>
-			</View>
+						{item.Name}
+					</Text>
+				)}
+				ListFooterComponent={
+					<TouchableOpacity onPress={nextPage}>
+						<Text>NEXT PAGE</Text>
+					</TouchableOpacity>
+				}
+				ListEmptyComponent={<Text>LOADING...</Text>}
+			/>
 		</View>
 	);
 }
@@ -42,90 +58,31 @@ const styles = StyleSheet.create({
 		backgroundColor: "#E8E8E8",
 		height: 808,
 		display: "flex",
-		justifyContent: "flex-end",
-		alignItems: "center",
 	},
-
-	innerContainer: {
-		backgroundColor: "orange", //"transparent"
-		height: 650,
-		marginBottom: 60,
-		display: "flex",
-		justifyContent: "space-between",
-		alignItems: "center",
-	},
-	buttonContainer: {
-		height: 180,
-		display: "flex",
-		justifyContent: "space-between",
-	},
-	innerContainerFirst: {
-		height: 798,
-		display: "flex",
-		justifyContent: "center",
-		alignItems: "center",
-	},
-
-	inputCont1: {
-		height: 640,
-		display: "flex",
-		justifyContent: "space-evenly",
-	},
-
-	innerContainerSecond: {
-		height: 808,
-		display: "flex",
-		justifyContent: "center",
-		alignItems: "center",
-	},
-	innerContainerHome: {
-		height: 808,
-		display: "flex",
-		justifyContent: "center",
-	},
-	Sview: {
-		height: 130,
-		backgroundColor: "transparent",
+	moodView: {
+		height: 90,
+		backgroundColor: "#E8E8E8",
 		shadowOffset: { width: -2, height: 3 },
 		shadowColor: "#c0c0c0",
 		shadowOpacity: 1.0,
 	},
-
-	formButton: {
-		height: 60,
-		width: 320,
-		backgroundColor: "#E8E8E8",
-		borderRadius: 20,
+	podsContainer: {
 		display: "flex",
-		justifyContent: "center",
-		shadowOffset: { width: 0, height: 3 },
-		shadowColor: "#c0c0c0",
-		shadowOpacity: 1.0,
-	},
-	baselineButton: {
-		height: 100,
-		backgroundColor: "#E8E8E8",
-		shadowOffset: { width: -2, height: -3 },
-		shadowColor: "#c0c0c0",
-		shadowOpacity: 1.0,
-	},
-	mainContainer: {
-		backgroundColor: "#E8E8E8",
-		height: 560,
+		flexDirection: "row",
+		flexWrap: "wrap",
+		justifyContent: "space-evenly",
+		backgroundColor: "#E8E8E8" /*"#E8E8E8"*/,
+		height: 610,
 		marginTop: 5,
 		shadowOffset: { width: -2, height: -3 },
 		shadowColor: "#c0c0c0",
 		shadowOpacity: 1.0,
 	},
-	moodCard: {
-		height: 90,
-		width: 90,
-		backgroundColor: "orange", //"#E8E8E8"
-		borderRadius: 20,
-		justifyContent: "center",
-		marginTop: 16,
-		marginLeft: 23,
-		shadowOffset: { width: 0, height: 3 },
+
+	baselineButton: {
+		height: 100,
+		backgroundColor: "#E8E8E8",
+		shadowOffset: { width: -2, height: -3 },
 		shadowColor: "#c0c0c0",
 		shadowOpacity: 1.0,
 	},
